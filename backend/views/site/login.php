2b7a591248711b8c30fe410e0057843e82f04e6e
Vetoni <a.zhaivoron@gmail.com>
2015-04-19 15:43:16 +0300

    логин + сжатие картинок при аплоаде

---
+++ backend/views/site/login.php
@@ -0,0 +1,27 @@
+<?php
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \common\models\LoginForm */
+
+$this->title = Yii::t('app', 'Login');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-login">
+    <p><?= Yii::t('app', 'Please fill out the following fields to login:') ?></p>
+
+    <div class="row">
+        <div class="col-lg-5">
+            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+            <?= $form->field($model, 'username') ?>
+            <?= $form->field($model, 'password')->passwordInput() ?>
+            <?= $form->field($model, 'rememberMe')->checkbox() ?>
+            <div class="form-group">
+                <?= Html::submitButton(Yii::t('app', 'Login'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            </div>
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+</div>
